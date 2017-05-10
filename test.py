dc4ec9af0eac77e5d85b8a26ef7b6ee44eeece8f
EliasVansteenkiste <Elias.Vansteenkiste@gmail.com>
2017-05-10 05:16:31 +0200

    test script

--- test.py
+++ test.py
@@ -3,18 +3,20 @@ import sys
 import lasagne as nn
 import numpy as np
 import theano
+import sklearn
+
+
 import buffering
 import pathfinder
 import utils
 from configuration import config, set_configuration
-from utils_plots import plot_slice_3d_3
-import utils_lung
 import logger
+import app
 
 theano.config.warn_float64 = 'raise'
 
 if len(sys.argv) < 2:
-    sys.exit("Usage: test.py <configuration_name>")
+    sys.exit("Usage: test.py <configuration_name> <'test'/'valid'>")
 
 config_name = sys.argv[1]
 set_configuration('configs', config_name)
@@ -65,22 +67,59 @@ givens_valid[model.l_target.input_var] = y_shared
 # theano functions
 iter_get_predictions = theano.function([], [valid_loss, nn.layers.get_output(model.l_out, deterministic=True)],
                                        givens=givens_valid)
-valid_data_iterator = config().valid_data_iterator
+
+
+valid = sys.argv[2] =='valid'
+test = sys.argv[2] == 'test'
+if valid:
+    data_iterator = config().valid_data_iterator
+elif test:
+    data_iterator = config().valid_data_iterator
 
 print
 print 'Data'
-print 'n validation: %d' % valid_data_iterator.nsamples
-threshold = 0.2
+print 'n', sys.argv[2], ': %d' % data_iterator.nsamples
+threshold = 0.5
 
 validation_losses = []
-for n, (x_chunk, y_chunk, id_chunk) in enumerate(buffering.buffered_gen_threaded(valid_data_iterator.generate())):
+preds = []
+targs = []
+for n, (x_chunk, y_chunk, id_chunk) in enumerate(buffering.buffered_gen_threaded(data_iterator.generate())):
     # load chunk to GPU
     x_shared.set_value(x_chunk)
     y_shared.set_value(y_chunk)
     loss, predictions = iter_get_predictions()
     validation_losses.append(loss)
-    targets = y_chunk[0, 0]
-    print id_chunk, targets, loss
+    targs.append(y_chunk)
+    preds.append(predictions)
+    #print id_chunk, targets, loss
+    if n%50 ==0:
+        print n, 'batches processed'
+
+preds = np.concatenate(preds)
+targs = np.concatenate(targs)
+
+print preds.shape
+print targs.shape
+
 
 print 'Validation loss', np.mean(validation_losses)
+if valid:
+    # weather_targs = []
+    # weather_preds = []
+    # for t in targs:
+    #     weather_targs.append(np.argmax(t[:4]))
+    # for p in preds:
+    #     weather_preds.append(np.argmax(p[:4]))
+    # print weather_preds[:10]
+    # print weather_targs[:10]
+    # print sklearn.metrics.confusion_matrix(weather_targs,weather_preds)
+
+    print 'Calculating F2 scores for each label seperately'
+    threshold = 0.5
+    qpreds = preds > threshold
+    print app.f2_score(targs[:,:17], qpreds[:,:17])
+    print app.f2_score(targs[:,:17], qpreds[:,:17], average=None)
+
+
 
